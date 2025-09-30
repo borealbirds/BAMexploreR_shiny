99707cd0e11c6d1b442bb58e0a4e2283b9ae1484
Melina Houle <houle.melina@gmail.com>
2025-09-29 22:40:46 -0400

    Add ggplot2 for pred tab

--- global.R
+++ global.R
@@ -2,8 +2,8 @@ options(repos = c(CRAN = "https://cran.rstudio.com"))
 
 required_packages <- c(
   "leaflet", "shiny", "rmarkdown", "markdown", "shinydashboard", "shinyjs", "shinyBS", 
-  "shinycssloaders",  "dplyr", "bslib", "leafem", "tmap", "glue", "purrr",
-  "terra", "stringr",  "DT",  "httr", "RColorBrewer", "sf", "tools", "viridis"
+  "shinycssloaders",  "dplyr", "bslib", "leafem", "glue", "purrr",
+  "terra", "stringr",  "DT",  "httr", "RColorBrewer", "sf", "tools", "viridis", "knitr", "ggplot2"
 )
 
 # Install any missing packages
@@ -15,14 +15,12 @@ if (length(missing_packages) > 0) {
 # Load the packages
 invisible(lapply(required_packages, library, character.only = TRUE))
 
-
 # data component related
 bcrv4.map <- vect('www/data/4326/BAM_BCRNMv4_4326.shp')
 bcrv5.map <- vect('www/data/4326/BAM_BCRNMv5_4326.shp')
 BCRNMv4 <- vect('www/data/5072/BAM_BCRNMv4_5072.shp')
 BCRNMv5 <- vect('www/data/5072/BAM_BCRNMv5_5072.shp')
 load("www/data/sysdata.rda")
-#load("www/data/BCRNM.RData")
 
 spp.grp <- c("COSEWIC","Cavity_Birds", "Waterfowl", "Marine_Birds","Shorebirds", "Wetland_Birds", "Birds_of_Prey",
              "Forest_Birds", "Grassland_Birds", "Aerial_Insectivores", "Arctic_Birds", "Long_Distance_Migrants")
@@ -31,6 +29,7 @@ spp.grp <- c("COSEWIC","Cavity_Birds", "Waterfowl", "Marine_Birds","Shorebirds",
 model.year <- c("2000","2005", "2010", "2015", "2020")
 
 spp_list <- read.csv('www/data/spp_List.csv')
+
 MB <- 1024^2
 
 UPLOAD_SIZE_MB <- 1000
@@ -49,7 +48,7 @@ for (file in base_module_files) source(file, local = TRUE)
 
 # The components that have modules. These names must match the values of the
 # tabs of the components in the UI.
-COMPONENTS <- c("data", "dist")
+COMPONENTS <- c("data", "popstats")
 
 # Information about modules that various parts of the app need access to
 COMPONENT_MODULES <- list()
